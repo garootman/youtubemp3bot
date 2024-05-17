7369504ebb32bdaa6797720aa3df09125e6f4b91
Dmitry Kozlov <106117111+garootman@users.noreply.github.com>
2024-05-17 21:36:52 +0400

    youtube oauth - later

--- app/ytlib.py
+++ app/ytlib.py
@@ -49,10 +49,13 @@ def download_audio(video_id, folder):
         print(f"File already exists: {filepath}")
         return filepath, "unknown"  # , None
 
+/home/garuda/VsRepo/youtubemp3bot/.venv/lib/python3.10/site-packages/pytube/__cache__/tokens.json
+
+
     while retries > 0:
         try:
-            yt = YouTube(url)
-            title = yt.title
+            yt = YouTube(url)  # , use_oauth = True , allow_oauth_cache = True)
+            title = str(yt.author) + ": " str(yt.title)
             # video_duration = yt.length
             yt.streams.get_audio_only().download(folder, filename=video_id + ".temp")
             os.rename(f"{folder}/{video_id}.temp", f"{folder}/{video_id}.mp4")
@@ -66,3 +69,12 @@ def download_audio(video_id, folder):
             retries -= 1
             time.sleep(1)
     raise Exception(f"Failed to download {url}: {msg}")
+
+
+if __name__ == "__main__":
+    # Test the download_audio function
+    restricted_video_url = "https://www.youtube.com/watch?v=Q_cZcXXWpIk"
+    x = YouTube(restricted_video_url, use_oauth = True , allow_oauth_cache = True)
+    print(dir(x))
+    print(x.title)
+    # download_audio("dQw4w9WgXcQ", "downloads")
