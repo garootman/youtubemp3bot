36467ac8a4f1b64c2d7582744bd7705efa2ba8f5
Dmitry Kozlov <106117111+garootman@users.noreply.github.com>
2024-06-04 22:39:05 +0400

    readme fix

--- runfile_nodocker.sh
+++
@@ -1,9 +0,0 @@
-# a shell file to run the whole pipeline without docker
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-
-python mybot.py
-
-celery -A worker.celery_app worker -l info --events
-celery -A worker.celery_app events
