a9ce23305b874d37e7f6460c156f20a5d8576465
Dmitry Kozlov <106117111+garootman@users.noreply.github.com>
2024-06-12 03:38:56 +0400

    rework woker

--- app/worker.py
+++ app/worker.py
@@ -39,129 +39,82 @@ pws = PaywallService()
 def process_task(task_id: str, cleanup=True):
     print("Worker called with task id", task_id)
     task = taskman.get_task_by_id(task_id)
-    done_task = taskman.lookup_task_by_media(
-        task.platform, task.media_type, task.media_id
+    if not task or not task.media_id:
+        print(f"Task {task_id} not found, or no media_id")
+        return
+
+    title, channel, duration, countries_yes, countries_no = yt_client.get_full_info(
+        task.media_id
     )
+    if not title:
+        print(f"Video not found or not available: {task.media_id}")
+        task.status = "NOTFOUND"
+        taskman.update_task(task)
+        send_msg(chat_id=task.chat_id, text="Video not found or not available")
+        return
 
-    x = []
-    dlmsg = None
-    error = ""
-    title = "unknown"
-    channel = "unknown"
-    duration = 0
-    filesize = 0
-    repeat = False
-    countries_yes = []
-    countries_no = []
-
-    if done_task:
-        print(f"Found same media in DB: task_id={done_task.id}")
-        x = mass_send_audio(
-            task.chat_id, done_task.tg_file_id.split(","), "MEDIA", done_task.title
-        )
-        if x:
-            repeat = True
-            title = done_task.title
-            channel = done_task.channel
-            duration = done_task.duration
-            countries_yes = done_task.countries_yes
-            countries_no = done_task.countries_no
-            filesize = done_task.filesize
-
-    if not x:
-        title, channel, duration = None, None, 0
-        dlmsg = send_msg(
-            chat_id=task.chat_id,
-            text="Downloading video. For large files it may take a while, please wait.",
-        )
-
-        print(
-            f"Downloading audio for url: {task.url}, type: {task.media_type}, id: {task.media_id}, platform: {task.platform}"
-        )
-        try:
-            title, channel, duration, countries_yes, countries_no = (
-                yt_client.get_full_info(task.media_id)
-            )
-            print(
-                f"Got title: {title}, channel: {channel}, duration: {duration}, countries_yes: {countries_yes}, countries_no: {countries_no}"
-            )
-
-            task.title = title
-            task.channel = channel
-            task.duration = duration
-            task.countries_yes = ",".join(countries_yes)
-            task.countries_no = ",".join(countries_no)
-            taskman.update_task(task)
-
-            if not title:
-                raise ValueError("Video not found or not available")
-
-            user_is_paid = pws.get_user_subscription(task.user_id)
-            print(f"User is paid: {user_is_paid}")
-            # olny paid users can download files longer than 30 minutes
-            if not user_is_paid and duration > FREE_MINUTES_MAX * 60:
-                send_msg(
-                    chat_id=task.chat_id,
-                    text="Video is over 30 minutes, /subscribe to download!",
-                )
-                task.status = "TOOLONG"
-                taskman.update_task(task)
-                return
-
-            proxy_url = proxy_mgr.get_checked_proxy_by_countries(
-                countries_yes, countries_no
-            )
-            print("Using proxy: ", proxy_url)
-
-            file_name = os.path.join(AUDIO_PATH, f"{task_id}.m4a")
-
-            filesize = download_audio(task.url, file_name, proxy=proxy_url)
-            if not filesize:
-                msg = f"Error downloading audio, file size is 0. Proxy was: {proxy_url}, countries_yes: {countries_yes}, countries_no: {countries_no}"
-                raise ValueError(msg)
-            size_mb = round(filesize / 1024 / 1024, 2)
-            print("Downloaded with size: ", size_mb, "MB to file: ", file_name)
-            # file_name, title, duration = download_audio(
-            #    task.url, task_id, AUDIO_PATH, proxy_url
-            # )
-
-            local_files, std, err = split_audio(
-                file_name, DURATION_STR, MAX_FILE_SIZE, FFMPEG_TIMEOUT
-            )
-            if err:
-                raise ValueError(f"Error splitting files: {err}")
-            x = mass_send_audio(task.chat_id, local_files, "FILE", title)
-
-            print("DONE!")
-        except Exception as e:
-            error = f"Error sending voice by downloading: {e}"
-            print(error)
-
-    if dlmsg:
-        delete_messages(task.chat_id, [dlmsg])
-
-    if x:
-        file_media_ids = ",".join([str(i.audio.file_id) for i in x if i])
-        task.status = "COMPLETE"
-        task.tg_file_id = file_media_ids
-        task.duration = duration
-        task.repeat = repeat
-        task.title = title
-        task.channel = channel
-        task.filesize = filesize
-        task.countries_yes = ",".join(countries_yes)
-        task.countries_no = ",".join(countries_no)
-
-    else:
-        task.status = "ERROR"
-        send_msg(chat_id=task.chat_id, text="Error sending voice, try again later")
-        send_msg(
-            chat_id=ADMIN_ID, text=f"Error sending msg for task {task_id}: {error}"
-        )
-
-    task.error = error
+    task.title = title
+    task.channel = channel
+    task.duration = duration
+    task.countries_yes = ",".join(countries_yes)
+    task.countries_no = ",".join(countries_no)
     taskman.update_task(task)
 
+    user_is_paid = pws.get_user_subscription(task.user_id)
+
+    if not user_is_paid and duration > FREE_MINUTES_MAX * 60:
+        send_msg(
+            chat_id=task.chat_id,
+            text="Video is over 30 minutes, /subscribe to download!",
+        )
+        task.status = "TOOLONG"
+        taskman.update_task(task)
+        return
+
+    proxy_url = proxy_mgr.get_checked_proxy_by_countries(countries_yes, countries_no)
+    print("Using proxy: ", proxy_url)
+
+    file_name = os.path.join(AUDIO_PATH, f"{task_id}.m4a")
+    filesize = download_audio(task.url, file_name, proxy=proxy_url)
+    if not filesize:
+        task.status = "ERROR"
+        taskman.update_task(task)
+        send_msg(
+            chat_id=task.chat_id, text="Error downloading audio, please try again later"
+        )
+        send_msg(chat_id=ADMIN_ID, text=f"Error at task {task_id}")
+        return
+
+    local_files, std, err = split_audio(
+        file_name, DURATION_STR, MAX_FILE_SIZE, FFMPEG_TIMEOUT
+    )
+    if not local_files:
+        task.status = "ERROR"
+        taskman.update_task(task)
+        send_msg(
+            chat_id=task.chat_id, text="Error downloading audio, please try again later"
+        )
+        send_msg(
+            chat_id=ADMIN_ID, text=f"Error splitting task {task_id}: \n\n{err}\n\n{std}"
+        )
+        return
+
+    x = mass_send_audio(task.chat_id, local_files, "FILE", title)
+    if not x:
+        task.status = "ERROR"
+        taskman.update_task(task)
+        send_msg(
+            chat_id=task.chat_id, text="Error downloading audio, please try again later"
+        )
+        send_msg(chat_id=ADMIN_ID, text=f"Error sending msg for task {task_id}")
+        return
+
+    task.status = "COMPLETE"
+    task.tg_file_id = ",".join([str(i.audio.file_id) for i in x if i])
+    task.filesize = filesize
+    task.countries_yes = ",".join(countries_yes)
+    task.countries_no = ",".join(countries_no)
+    taskman.update_task(task)
     if cleanup:
         _ = delete_files_by_chunk(AUDIO_PATH, task_id)
 
