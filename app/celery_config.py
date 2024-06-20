c738bd25198bbe2f97eccd7d3f2e9b01f8750cae
Dmitry Kozlov <106117111+garootman@users.noreply.github.com>
2024-06-21 01:15:44 +0400

    concurrency 1

--- app/celery_config.py
+++ app/celery_config.py
@@ -8,7 +8,7 @@ celery_app = Celery("worker", broker=REDIS_URL, backend=REDIS_URL)
 celery_app.autodiscover_tasks(["worker"])
 
 
-celery_app.conf.update(result_expires=3600, worker_concurrency=10)
+celery_app.conf.update(result_expires=3600, worker_concurrency=4)
 celery_app.conf.worker_send_task_events = True
 celery_app.conf.task_send_sent_event = True
 celery_app.conf.timezone = "UTC"
