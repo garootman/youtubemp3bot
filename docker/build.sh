e04fa81af7fa3322b2f4e0a3facb306f59732e1d
Dmitry Kozlov <106117111+garootman@users.noreply.github.com>
2024-06-23 00:40:20 +0400

    pytest to dockerfiles

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,30 @@
-docker build -t tgmediabot -f Dockerfile_tgbot ..
-docker build -t tgmediawrk -f Dockerfile_worker ..
+#!/bin/bash
+
+# Load environment variables from .env file
+export $(grep -v '^#' ../.env | xargs)
+
+# Build the Docker image for bot
+docker build -t mytgbot \
+  --build-arg POSTGRES_URL=$POSTGRES_URL \
+  --build-arg REDIS_URL=$REDIS_URL \
+  --build-arg REDIS_PASSWORD=$REDIS_PASSWORD \
+  --build-arg TG_TOKEN=$TG_TOKEN \
+  --build-arg ADMIN_ID=$ADMIN_ID \
+  --build-arg PROXY_TOKEN=$PROXY_TOKEN \
+  --build-arg ENV=$ENV \
+  --build-arg GOOGLE_API_KEY=$GOOGLE_API_KEY \
+  --build-arg PAY_LINK=$PAY_LINK \
+  -f Dockerfile_tgbot ..
+
+# Build the Docker image for worker
+docker build -t mytgbot \
+  --build-arg POSTGRES_URL=$POSTGRES_URL \
+  --build-arg REDIS_URL=$REDIS_URL \
+  --build-arg REDIS_PASSWORD=$REDIS_PASSWORD \
+  --build-arg TG_TOKEN=$TG_TOKEN \
+  --build-arg ADMIN_ID=$ADMIN_ID \
+  --build-arg PROXY_TOKEN=$PROXY_TOKEN \
+  --build-arg ENV=$ENV \
+  --build-arg GOOGLE_API_KEY=$GOOGLE_API_KEY \
+  --build-arg PAY_LINK=$PAY_LINK \
+  -f Dockerfile_worker ..
